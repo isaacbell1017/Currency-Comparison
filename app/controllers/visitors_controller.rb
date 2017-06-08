c741eb3909541067103575c1662b98db8af70fe7
isaacbell1017 <isaacbell1017@gmail.com>
2017-06-08 21:26:43 +0000

    Basic page layout and chart setup

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+  # GET "/"
+  def index
+    @btc_data = BitcoinPriceIndex.get_historical_range_between_dates(1.year.ago, 1.day.ago).to_json
+    @eth_data = EthereumPriceIndex.get_price_history.to_json
+    @nasdaq   = NasdaqGlobalIndex.get_index.to_json
+  end
 end
