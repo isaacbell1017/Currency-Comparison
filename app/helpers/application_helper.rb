75f388a4005a4741a973bf35aa02b95acfe5b541
isaacbell1017 <isaacbell1017@gmail.com>
2017-06-09 02:22:01 +0000

    Finalize layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
     html = '[[' + raw(fields.join('], [')) + ']]'
     return html
   end
+  
+  def ethereum_data(data)
+    fields = []
+    data.each do |datum|
+      date = datum[0].split('T')[0].split('-').join(', ')
+      fields += ["Date.UTC(#{date}), #{datum[1].to_s}"]
+    end
+    
+    html = '[[' + raw(fields.join('], [')) + ']]'
+    return html
+  end
+  
+  def nasdaq_data(data)
+    fields = []
+    data.each do |datum|
+      date = datum[0].split('-').join(', ')
+      fields += ["Date.UTC(#{date}), #{datum[1].to_s}"]
+    end
+    
+    html = '[[' + raw(fields.join('], [')) + ']]'
+    return html
+  end
 end
